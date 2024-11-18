79c7a0963888b6f90e01a01e1e4eca96b83ca3d3
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-18 21:18:19 +0100

    rename store to 'WeatherStore'

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 
-type CharactersStore = {
+type WeatherStore = {
   location: string | null;
   selectLocation: (location: string) => void;
   fetchWeatherData: () => Promise<void>;
 };
 
-export const useCharactersStore = create<CharactersStore>((set) => ({
+export const useWeatherStore = create<WeatherStore>((set) => ({
   location: null,
   selectLocation: (selectedLocation) => set({ location: selectedLocation }),
   fetchWeatherData: async () => {},
