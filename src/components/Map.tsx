1ad5de3da86a95233077e06542bec0f2820bb54d
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-19 19:08:23 +0100

    implement geolocation and manual location selection on the map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,49 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents, useMap } from "react-leaflet";
+import { useWeatherStore } from "../store/store";
 
 export function AppMap() {
-  return (
-    <MapContainer id="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>Wow! A nice popup!</Popup>
+  const { coordinates, selectCoordinates } = useWeatherStore();
+
+  function LocationMarker() {
+    const map = useMap();
+    useMapEvents({
+      click(e) {
+        selectCoordinates([e.latlng.lat, e.latlng.lng]);
+        console.log(`Selected location: lat ${e.latlng.lat} • long ${e.latlng.lng}`);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+      locationfound(e) {
+        selectCoordinates([e.latlng.lat, e.latlng.lng]);
+        console.log(`Selected location: lat ${coordinates?.[0]} • long ${coordinates?.[1]}`);
+      },
+    });
+
+    return location ? (
+      <Marker position={coordinates ?? [0, 0]}>
+        <Popup>
+          Position : lat:{coordinates?.[0]}, long:{coordinates?.[1]}
+        </Popup>
       </Marker>
-    </MapContainer>
+    ) : null;
+  }
+
+  const handleGeolocate = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      selectCoordinates([position.coords.latitude, position.coords.longitude]);
+      console.log("Detected location:", position.coords);
+    });
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleGeolocate}>
+        Geolocate Me!
+      </button>
+      <MapContainer id="map" center={[43.533329, 5.43333]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <LocationMarker />
+      </MapContainer>
+    </>
   );
 }
