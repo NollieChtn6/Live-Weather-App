b89832151c92c4666637b4a0526d771876805db6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-19 19:11:17 +0100

    remove LocationSelectorForm

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import { LocationSelectorForm } from "../components/LocationSelectorForm";
 import { AppMap } from "../components/Map";
 import { Dashboard } from "../components/Dashboard";
 
@@ -8,15 +7,7 @@ export function HomePage() {
       <h1>My Tiny Weather App</h1>
       <div className="content-container">
         <div className="settings-container">
-          <h2>Settings</h2>
-          <div className="settings-select">
-            <select id="select-options" name="select-options">
-              <option disabled>Select an option</option>
-              <option>Select location manually</option>
-              <option>Use map</option>
-            </select>
-          </div>
-          <LocationSelectorForm />
+          <h2>Location</h2>
           <AppMap />
         </div>
         <div className="data-container">
