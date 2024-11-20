6aad244087d9ca490db29e0debb4664abd4b6a36
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-20 23:25:51 +0100

    fetch weather data from Visual Crossing Weather API using axios

---
+++ src/services/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import type { WeatherAPIResponseType } from "../@types/types";
+
+/**
+ * Fetch weather data from the Visual Crossing Weather API.
+ * @param lat - Latitude
+ * @param lon - Longitude
+ * @returns WeatherAPIResponseType
+ */
+
+const API_URL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+const API_KEY = import.meta.env.VITE_VISUAL_CROSSING_API_KEY as string;
+
+export const getWeatherData = async (lat: number, lon: number): Promise<WeatherAPIResponseType> => {
+  try {
+    const response = await axios.get<WeatherAPIResponseType>(
+      `${API_URL}/${lat},${lon}/today?unitGroup=metric&key=${API_KEY}`,
+    ); //&contentType=json
+    return response.data;
+  } catch (error) {
+    console.error("API error: ", error);
+    throw new Error("Failed to fetch weather data. Please try again.");
+  }
+};
