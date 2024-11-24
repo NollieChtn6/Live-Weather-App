42bc94a984c36a82f6852e053f7c212b10317e16
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-24 15:34:17 +0100

    create 'colors' and 'fontSize' variables as objects

---
+++ src/variables.css.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  background: "#0F0616",
+  primary: "#31293F",
+  secondary: "#554D74",
+  tertiary: "#796EA8",
+  accent: "#A6A9C8",
+  light: "#796EA8",
+};
+
+export const fontSize = {
+  h1: "2rem",
+  h2: "1.5rem",
+  h3: "1.25rem",
+};
